6644d264aeda19093989f68637bdbafc8e709a02
Saransh <saransh0701@gmail.com>
2021-12-05 01:48:20 +0530

    Comments

--- train.py
+++ train.py
@@ -1,35 +1,41 @@
 import os
-import pickle
 import numpy as np
 import splitfolders
 from glob import glob
 from PIL import Image
 import matplotlib.pyplot as plt
 from keras.models import Sequential
+from keras.models import load_model
 from keras.applications.vgg19 import VGG19
 from keras.layers import Input, Lambda, Dense, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 
 
+# train test cross-validation split
 if not os.path.exists(os.path.join(os.getcwd(), 'output')):
     splitfolders.ratio("indoorCVPR_09/Images", output="output",
                        seed=1337, ratio=(.7, .2, .1), group_prefix=None)
 
+# calculate number of categories
 folders = glob('indoorCVPR_09/Images/*')
 categories = [folder.split("\\")[-1] for folder in folders]
 
+# create a labels dict for ease
 labels = {}
 label_no = 0
 for category in categories:
     labels[label_no] = category
     label_no += 1
 
+# create a VGG19 net
 vgg19 = VGG19(input_shape=(224, 224, 3), weights='imagenet', include_top=False)
 vgg19.trainable = False
 
+# create extra layers for output
 flatten_layer = Flatten()
 prediction_layer = Dense(len(folders), activation='softmax')
 
+# create the model
 model = Sequential([
     vgg19,
     flatten_layer,
@@ -39,12 +45,15 @@ model = Sequential([
 # view the structure of the model
 model.summary()
 
+# compile with adam
 model.compile(
     loss='categorical_crossentropy',
     optimizer='adam',
     metrics=['accuracy'],
 )
 
+# create data generators for training and validation data
+# testing has been done in the n`train.ipynb` notebook
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     shear_range=0.2,
@@ -56,6 +65,7 @@ val_datagen = ImageDataGenerator(
     rescale=1./255,
 )
 
+# bring in the data
 training_set = train_datagen.flow_from_directory(
     'output/train/',
     target_size=(224, 224),
@@ -70,6 +80,7 @@ validation_set = val_datagen.flow_from_directory(
     class_mode='categorical'
 )
 
+# fit the data
 model.fit(
     training_set,
     validation_data=validation_set,
@@ -77,6 +88,5 @@ model.fit(
     batch_size=32,
 )
 
-from keras.models import load_model
-
+# save the model
 model.save('model.h5')

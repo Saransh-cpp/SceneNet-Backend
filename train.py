65acb419c60d0b2ea63facb26c9921ecdc777589
Saransh <saransh0701@gmail.com>
2021-12-05 01:23:54 +0530

    Train the model completely

--- train.py
+++ train.py
@@ -1,13 +1,29 @@
-import cv2
+import os
+import pickle
+import numpy as np
+import splitfolders
 from glob import glob
 from PIL import Image
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.applications.vgg19 import VGG19
 from keras.layers import Input, Lambda, Dense, Flatten
 from keras.preprocessing.image import ImageDataGenerator
 
 
+if not os.path.exists(os.path.join(os.getcwd(), 'output')):
+    splitfolders.ratio("indoorCVPR_09/Images", output="output",
+                       seed=1337, ratio=(.7, .2, .1), group_prefix=None)
+
 folders = glob('indoorCVPR_09/Images/*')
+categories = [folder.split("\\")[-1] for folder in folders]
+
+labels = {}
+label_no = 0
+for category in categories:
+    labels[label_no] = category
+    label_no += 1
+
 vgg19 = VGG19(input_shape=(224, 224, 3), weights='imagenet', include_top=False)
 vgg19.trainable = False
 
@@ -23,35 +39,44 @@ model = Sequential([
 # view the structure of the model
 model.summary()
 
-# tell the model what cost and optimization method to use
 model.compile(
     loss='categorical_crossentropy',
     optimizer='adam',
     metrics=['accuracy'],
 )
 
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
 
-train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(
+    rescale=1./255,
+)
 
-test_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+training_set = train_datagen.flow_from_directory(
+    'output/train/',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
 
-training_set = train_datagen.flow_from_directory('indoorCVPR_09/Images/',
-                                                 target_size=(224, 224),
-                                                 batch_size=32,
-                                                 subset="training",
-                                                 class_mode='categorical')
-
-test_set = test_datagen.flow_from_directory('indoorCVPR_09/Images/',
-                                            target_size=(224, 224),
-                                            batch_size=32,
-                                            subset="validation",
-                                            class_mode='categorical')
+validation_set = val_datagen.flow_from_directory(
+    'output/val/',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
 
 model.fit(
     training_set,
-    validation_data=test_set,
-    epochs=5,
+    validation_data=validation_set,
+    epochs=10,
     batch_size=32,
 )
 
-model.predict(cv2.imread(r"indoorCVPR_09\Images\airport_inside\airport_inside_0001.jpg"))
+from keras.models import load_model
+
+model.save('model.h5')
